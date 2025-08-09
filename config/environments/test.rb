ba36d7437167a88dfff29f32b55fd0e2d432d57e
apsa <pauloserpaantunes@gmail.com>
2025-08-09 15:18:54 -0300

    Remove css compilation from tests

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -50,4 +50,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Evita a compilação de assets nos testes
+  config.assets.compile = false
+  config.assets.digest = true
+  config.assets.debug = false
 end
